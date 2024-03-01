f3af3529188a56ded7e66a2a532d3f7b49d507e6
emily <emilycassel77@gmail.com>
2024-03-01 14:52:39 -0500

    fixed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,31 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'PWA Text Editor',
+        inject: true,
+        fingerprints: false,
+        short_name: 'PWA Text Editor',
+        description: 'A simple text editor that uses PWA technologies',
+        background_color: '#ffffff',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
